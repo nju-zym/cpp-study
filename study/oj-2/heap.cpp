a6676dafe22badd46ec28328cfd12cc11b47889e
nju-zym <241880200@smail.nju.edu.cn>
2025-03-29 14:55:03 +0800

    修复heap的bug

--- study/oj-2/heap.cpp
+++ study/oj-2/heap.cpp
@@ -1,8 +1,9 @@
-#include"heap.hpp"
+#include "heap.hpp"
 
-#include"MM.hpp"
+#include "MM.hpp"
 
-#include<iostream>
+#include <cstddef>
+#include <iostream>
 
 using std::cout;
 
@@ -35,8 +36,8 @@ void Heap::dilatation() {
     while (temp->nxt != nullptr) {
         temp = temp->nxt;
     }
-    size_t old_size = Heap::size;
-    Heap::size *= 2;
+    size_t old_size  = Heap::size;
+    Heap::size      *= 2;
     for (int i = 0; i < old_size / 4; i++) {
         MM_Struct* next = new MM_Struct();
         temp->nxt       = next;
@@ -45,40 +46,73 @@ void Heap::dilatation() {
 }
 MM_Struct* Heap::Malloc(size_t target_size) {
     //TODO
-    int targrt = target_size / 4;
-    MM_Struct* begin   = head;
-    MM_Struct* check   = nullptr;
+    int target = target_size / 4;
+    if (target == 0) {
+        return nullptr;
+    }
+
     while (true) {
-        while (begin != nullptr) {
-            if (begin->locked == true) {
-                begin = begin->nxt;
-                continue;
-            }
-            check = begin;
-            for (int i = 1; i < targrt; i++) {
-                if (check->nxt == nullptr || check->locked == true) {
-                    begin = check->nxt;
-                    check   = nullptr;
+        MM_Struct* current = head;
+        MM_Struct* start   = nullptr;
+        while (current != nullptr) {
+            if (!current->locked) {
+                MM_Struct* check = current;
+
+                bool flag = true;
+                for (size_t i = 0; i < target; i++) {
+                    if (check == nullptr || check->locked == true) {
+                        flag = false;
+                        break;
+                    }
+                    check = check->nxt;
+                }
+                if (flag) {
+                    start = current;
                     break;
                 }
-                check = check->nxt;
             }
-            if (check != nullptr) {
-                begin->size     = target_size;
-                MM_Struct* temp = begin;
-                for (int i = 1; i <= targrt; i++) {
-                    temp->locked = true;
-                    temp         = temp->nxt;
-                }
-                return begin;
-            }
-            begin = begin->nxt;
+            current = current->nxt;
         }
-        dilatation();
-        begin = head;
-        check  = nullptr;
+        if (start != nullptr) {
+            start->size     = target_size;
+            MM_Struct* temp = start;
+            for (size_t i = 0; i < target; i++) {
+                temp->locked = true;
+                temp         = temp->nxt;
+            }
+            return start;
+        } else {
+            dilatation();
+        }
     }
 }
+
+// if (begin->locked == true) {
+//     begin = begin->nxt;
+//     continue;
+// }
+// check = begin;
+// for (int i = 1; i < targrt; i++) {
+//     if (check->nxt == nullptr || check->locked == true) {
+//         begin = check->nxt;
+//         check   = nullptr;
+//         break;
+//     }
+//     check = check->nxt;
+// }
+// if (check != nullptr) {
+//     begin->size     = target_size;
+//     MM_Struct* temp = begin;
+//     for (int i = 1; i <= targrt; i++) {
+//         temp->locked = true;
+//         temp         = temp->nxt;
+//     }
+//     return begin;
+// }
+// if (begin == nullptr) {
+//     break;
+// }
+// begin = begin->nxt
 void Heap::Free(MM_Struct* p) {
     //TODO
     if (p == nullptr) {
@@ -93,57 +127,100 @@ void Heap::Free(MM_Struct* p) {
         cout << "Error Free!\n";
         return;
     }
-    if (p == head){
-        MM_Struct* p_end = p;
-        int length = p->size / 4;
-        for (int i = 1; i < length; i++) {
-            p_end = p_end->nxt;
-            p_end->locked = false;
-            p_end->val = 0;
-        }
-        p->locked = false;
-        p->val = 0;
-        p->size = 0;
-        
-        if (p_end->nxt == nullptr) {
-            return;
-        }
+    size_t target = p->size / 4;
+    MM_Struct* p_end = p;
+    MM_Struct* prev = nullptr;
 
-        head = p_end->nxt;
-        MM_Struct* end = head;
-        while (end->nxt != nullptr) {
-            end = end->nxt;
-        }
-        end->nxt = p;
-        p_end->nxt = nullptr;
-        return;
+    MM_Struct* find = head;
+    while (find != nullptr && find != p) {
+        prev = find;
+        find = find->nxt;
     }
-    MM_Struct* pre = head;
-    while (pre->nxt != nullptr && pre->nxt != p) {
-        pre = pre->nxt;
-    }
-    MM_Struct* p_end  = p;
-    int length = p->size / 4;
-    for (int i = 1; i < length; i++) {
+
+    for (size_t i = 1; i < target; i++){
         p_end = p_end->nxt;
-        p_end->locked = false;
-        p_end->val = 0;
     }
-    p->locked = false;
-    p->val = 0;
-    p->size = 0;
+
+    MM_Struct* resetter = p;
+    for (size_t i = 0; i < target; i++) {
+        resetter->locked = false;
+        if (i == 0){
+            resetter->size = 0;
+        }
+        resetter = resetter->nxt;
+    }
     if (p_end->nxt == nullptr) {
         return;
     }
-    pre->nxt = p_end->nxt;
-    MM_Struct* end = head;
-    while (end->nxt != nullptr){
-        end = end->nxt;
+
+    MM_Struct* after = p_end->nxt;
+
+    MM_Struct* tail = head;
+
+    while (tail->nxt != nullptr) {
+        tail = tail->nxt;
     }
-    end->nxt = p;
+
+    if (prev == nullptr) {
+        head = after;
+    } else {
+        prev->nxt = after;
+    }
+
+    tail->nxt = p;
     p_end->nxt = nullptr;
 }
+    // if (p == head) {
+    //     MM_Struct* p_end = p;
+    //     int length       = p->size / 4;
+    //     for (int i = 1; i < length; i++) {
+    //         p_end         = p_end->nxt;
+    //         p_end->locked = false;
+    //         p_end->val    = 0;
+    //     }
+    //     p->locked = false;
+    //     p->val    = 0;
+    //     p->size   = 0;
 
+    //     if (p_end->nxt == nullptr) {
+    //         return;
+    //     }
+
+    //     head           = p_end->nxt;
+    //     MM_Struct* end = head;
+    //     while (end->nxt != nullptr) {
+    //         end = end->nxt;
+    //     }
+    //     end->nxt   = p;
+    //     p_end->nxt = nullptr;
+    //     return;
+    // }
+    // MM_Struct* pre = head;
+    // while (pre->nxt != nullptr && pre->nxt != p) {
+    //     pre = pre->nxt;
+    // }
+    // MM_Struct* p_end = p;
+    // int length       = p->size / 4;
+    // for (int i = 1; i < length; i++) {
+    //     p_end         = p_end->nxt;
+    //     p_end->locked = false;
+    //     p_end->val    = 0;
+    // }
+    // p->locked = false;
+    // p->val    = 0;
+    // p->size   = 0;
+    // if (p_end->nxt == nullptr) {
+    //     return;
+    // }
+
+    // pre->nxt   = p_end->nxt;
+    // p_end->nxt = nullptr;
+
+    // MM_Struct* end = head;
+    // while (end->nxt != nullptr) {
+    //     end = end->nxt;
+    // }
+    // end->nxt = p;
 
 void Heap::output(MM_Struct* p) {
     //TODO
@@ -155,16 +232,15 @@ void Heap::output(MM_Struct* p) {
         cout << "Segmentation Fault!\n";
         return;
     }
-    if (p->size != 0){
-        int length = p->size / 4;
+    if (p->size != 0) {
+        int length      = p->size / 4;
         MM_Struct* temp = p;
         for (int i = 0; i < length; i++) {
-            cout << temp->val << ' ';
+            cout << temp->val << (i == length - 1 ? "" : " ");
             temp = temp->nxt;
         }
         cout << '\n';
-    }
-    else {
+    } else {
         cout << p->val << '\n';
     }
 }
@@ -201,10 +277,12 @@ static void test2() {
     std::cout << heap->GetTotalSize() << '\n';
     delete heap;
 }
-int main(){
+int main() {
     Heap* heap   = new Heap(12);
     MM_Struct* p = heap->Malloc(12);
-    
+    heap->setval(p, 1);
+    heap->setval(p->nxt, 2);
+    heap->setval(p->nxt->nxt, 3);
     heap->output(p);
     heap->output(p->nxt);
     heap->output(nullptr);
